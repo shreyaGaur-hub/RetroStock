75611acc920aae942f0414eac4002931c0ccff82
Shreyas Rajmohan <shreyasrajmohanstudy@gmail.com>
2026-02-09 12:14:23 +0530

    Update simulationEngine.js

--- simulationEngine.js
+++ simulationEngine.js
@@ -3,6 +3,7 @@ import { getMonthlyReturn, getEvent } from "./historicalData.js";
 export function simulateMonth(currentDate, portfolio) {
   const monthlyReturn = getMonthlyReturn(currentDate);
 
+  // Calculate new stock value based on monthly return
   const updatedStocks = portfolio.stocks * (1 + monthlyReturn);
 
   const updatedPortfolio = {
